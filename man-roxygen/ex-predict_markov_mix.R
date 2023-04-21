aaf54e813296c4cab8116f1d67f3730465da702c
zhuxr11 <zxr6@163.com>
2023-04-21 23:26:11 +0800

    Reformat lines too long in examples.

--- man-roxygen/ex-predict_markov_mix.R
+++ man-roxygen/ex-predict_markov_mix.R
@@ -4,7 +4,10 @@ data("markov_mix_ex")
 # Generate a new list of sequences
 set.seed(2222L)
 new_maxlen <- 8L
-new_seq_list <- purrr::rerun(.n = 50L, sample(markov_mix_ex[["states"]], sample.int(new_maxlen, 1L), replace = TRUE))
+new_seq_list <- purrr::rerun(
+  .n = 50L,
+  sample(markov_mix_ex[["states"]], sample.int(new_maxlen, 1L), replace = TRUE)
+)
 
 # Predict the probabilities of sequences
 predict(markov_mix_ex, newdata = new_seq_list)
